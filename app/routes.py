07eaf34f26f7286bffcdcf3dd420c9144e681257
Thomas Hamilton <88941733+tlh45342@users.noreply.github.com>
2021-09-26 21:14:12 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,12 @@ from app import app
 def index():
     return render_template("welcome.html", is_authenticated=current_user.is_authenticated)
 
-
 @app.route("/protected",)
 @login_required
 def protected():
     return render_template("protected.html")
+
+
+@app.errorhandler(404)
+def invalid_route(e):
+  return render_template("404.html")
